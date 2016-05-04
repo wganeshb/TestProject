1e99fa7a4fc0cc3e7bd208bb9a31b2ceefef705f
Ganesh Waghmode <Ganesh@ganeshs-imac.local>
2016-05-04 12:23:37 +0530

    Initial Commit

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by Ganesh Waghmode on 04/05/16.
+//  Copyright © 2016 Ganesh Waghmode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
